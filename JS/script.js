3cd3227a20e4de87e42939c1974c03c64c5864b0
emmanuelle_dev <erdmann.emmanuelle@gmail.com>
2023-07-07 20:42:21 +0200

    Les 2 joueurs jouent en simultané.
    Ajouts:
    . Quand un joueur presse hold, c'est à l'autre joueur.
    . Quand le dé est sur 1, c'est à l'autre joueur.

--- JS/script.js
+++ JS/script.js
@@ -9,16 +9,58 @@ let btnNewGame = document.getElementById('newGame');
 let btnHold = document.getElementById('hold');
 
 
-btnRoll.addEventListener('click', () => {
-    nb = Math.floor(Math.random()*6)+1;
-        if (nb == 1){
+btnRoll.addEventListener('click', rollDicePlayer1);
+    function rollDicePlayer1(){
+        nb = Math.floor(Math.random()*6)+1;
+
+        if (nb > 1) {
+            scoreRound1 = scoreRound1 + nb;
+            document.getElementById('round1').innerHTML = scoreRound1;
+            document.getElementById('player1').style.backgroundColor='#ffffff';
+        } else if (nb == 1){
+            scoreRound1 = 0;
+            document.getElementById('player1').style.backgroundColor='#808080';
+            document.getElementById('round1').innerHTML = scoreRound1;
+            btnRoll.removeEventListener('click', rollDicePlayer1);
+            btnRoll.addEventListener('click', rollDicePlayer2);
+        }   
+
+        btnHold.addEventListener('click', holdPlayer1);
+        function holdPlayer1 (){
             scoreRound1 = 0;
             document.getElementById('round1').innerHTML = scoreRound1;
-        } else if (nb > 1) {
-            scoreRound1 = scoreRound1 + nb;
-        }    
-    document.getElementById('round1').innerHTML = scoreRound1;
-})
+            document.getElementById('player1').style.backgroundColor='#808080';
+            btnRoll.removeEventListener('click', rollDicePlayer1);
+            btnRoll.addEventListener('click', rollDicePlayer2);
+        }
+    }
+
+
+    function rollDicePlayer2(){
+        nb = Math.floor(Math.random()*6)+1;
+
+        if (nb > 1) {
+            scoreRound2 = scoreRound2 + nb;
+            document.getElementById('round2').innerHTML = scoreRound2;
+            document.getElementById('player2').style.backgroundColor='#ffffff';
+        } else if (nb == 1){
+            scoreRound2 = 0;
+            document.getElementById('player2').style.backgroundColor='#808080';
+            document.getElementById('round2').innerHTML = scoreRound2;
+            btnRoll.removeEventListener('click', rollDicePlayer2);
+            btnRoll.addEventListener('click', rollDicePlayer1);
+        }   
+
+        btnHold.addEventListener('click', holdPlayer2);
+        function holdPlayer2 (){
+            scoreRound2 = 0;
+            document.getElementById('round2').innerHTML = scoreRound2;
+            document.getElementById('player2').style.backgroundColor='#808080';
+            btnRoll.removeEventListener('click', rollDicePlayer2);
+            btnRoll.addEventListener('click', rollDicePlayer1);
+        }
+    }    
+
 
 btnHold.addEventListener('click', () => { 
     if (scoreGlobal1 > 99){
@@ -29,11 +71,26 @@ btnHold.addEventListener('click', () => {
         scoreGlobal1 = scoreGlobal1 + scoreRound1;
         document.getElementById('global1').innerHTML = scoreGlobal1;
     }
+    if (scoreGlobal2 > 99){
+        scoreRound2 = 0;
+        document.getElementById('round2').innerHTML = scoreRound2;
+        document.getElementById('global2').innerHTML = "Winner";
+    } else if (scoreGlobal2 < 100) {
+        scoreGlobal2 = scoreGlobal2 + scoreRound2;
+        document.getElementById('global2').innerHTML = scoreGlobal2;
+    }
 })
+
 
 btnNewGame.addEventListener('click', () => {
     scoreRound1 = 0;
     scoreGlobal1 = 0;
+    scoreRound2 = 0;
+    scoreGlobal2 = 0;
     document.getElementById('round1').innerHTML = scoreRound1;
     document.getElementById('global1').innerHTML = scoreGlobal1;
+    document.getElementById('round2').innerHTML = scoreRound2;
+    document.getElementById('global2').innerHTML = scoreGlobal2;
+    document.getElementById('player1').style.backgroundColor='#ffffff';
+    document.getElementById('player2').style.backgroundColor='#ffffff';
 })
